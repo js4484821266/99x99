26047380b675402a71116967aee72806621d425d
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2024-08-11 20:29:23 +0900

    print a pair of 2-digit numbers

--- js.js
+++ js.js
@@ -1,5 +1,13 @@
-/*function get_pair() {
-    var a = Math.floor(Math.random() * 100);
-    var b = Math.floor(Math.random() * 100);
-    document.getElementById("operation").innerHTML = a + "&times;" + b + "=";
-}*/
+var lmn_a, lmn_b;
+function get_pair() {
+    lmn_a = document.getElementById("a");
+    lmn_b = document.getElementById("b");
+}
+function set_pair() {
+    lmn_a.innerHTML = Math.floor(Math.random() * 100);
+    lmn_b.innerHTML = Math.floor(Math.random() * 100);
+}
+window.onload = function () {
+    get_pair();
+    set_pair();
+}
