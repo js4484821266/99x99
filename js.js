783cf455497fce8f15bf59c9ef40d1b26b2f8b2b
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2024-08-11 21:05:19 +0900

    let check if correct

--- js.js
+++ js.js
@@ -1,13 +1,27 @@
-var lmn_a, lmn_b;
-function get_pair() {
-    lmn_a = document.getElementById("a");
-    lmn_b = document.getElementById("b");
-}
+var lmn_a, lmn_b,lmnoa,lmnob,rspns,crrct,answr,a,b;
 function set_pair() {
-    lmn_a.innerHTML = Math.floor(Math.random() * 100);
-    lmn_b.innerHTML = Math.floor(Math.random() * 100);
+    a = Math.floor(Math.random() * 100);
+    b = Math.floor(Math.random() * 100);
+    lmn_a.innerHTML = a;
+    lmn_b.innerHTML = b;
 }
 window.onload = function () {
-    get_pair();
+    lmn_a = document.getElementById("a");
+    lmn_b = document.getElementById("b");
+    lmnoa = document.getElementById("old_a");
+    lmnob = document.getElementById("old_b");
+    rspns = document.getElementById("response");
+    crrct = document.getElementById("correct");
+    answr = document.getElementById("answer");
+    set_pair();
+}
+function submit(){
+    lmnoa.innerHTML = a;
+    lmnob.innerHTML= b;
+    crrct.innerHTML = a*b;
+    // print the guessing in "response" if incorrect
+    if (answr.value != a*b){
+        rspns.innerHTML = answr.value;
+    }
     set_pair();
 }
