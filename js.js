3caf1514b6daeffb718d623c64bdcde9f08dd61a
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2024-08-11 19:49:42 +0900

    first commit

---
+++ js.js
@@ -0,0 +1,5 @@
+/*function get_pair() {
+    var a = Math.floor(Math.random() * 100);
+    var b = Math.floor(Math.random() * 100);
+    document.getElementById("operation").innerHTML = a + "&times;" + b + "=";
+}*/
